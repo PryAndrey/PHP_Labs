4aa6648c23d784aeec59b12394a38c934ce88055
Андрей Пряшников <prandruha2003@yandex.ru>
2023-05-20 20:36:55 +0300

    Стили

--- Pizza_service/public/script.js
+++ Pizza_service/public/script.js
@@ -1,70 +1,81 @@
 var products = new Map()
 var cost = 0
 var productString = ""
+var basketString = ""
+
+function showBasket() {
+  basketString = "";
+  for (let pair of products.entries()) {
+    basketString += `${pair[0]} x${pair[1]}<br/>`
+  }
+  return basketString
+}
 
 function addProduct(pizzaTitle, pizzaCost) {
   if (products.has(pizzaTitle)) {
-    products.set(pizzaTitle, products.get(pizzaTitle) + 1)
+    products.set(pizzaTitle, products.get(pizzaTitle) + 1);
   } else {
-    products.set(pizzaTitle, 1)
+    products.set(pizzaTitle, 1);
   }
-  document.getElementById(pizzaTitle).innerHTML = "В корзине: " + products.get(pizzaTitle)
-  cost += pizzaCost
-  console.log(products)
+  document.getElementById(pizzaTitle).innerHTML = "В корзине: " + products.get(pizzaTitle);
+  cost += pizzaCost;
+  document.getElementById('basket').innerHTML = showBasket();
+
+  document.getElementById("orderBtn").classList.remove('unVisibleDisplay');
 }
+
 function deleteProduct(pizzaTitle, pizzaCost) {
   if (products.has(pizzaTitle)) {
     if (products.get(pizzaTitle) === 1) {
-      products.delete(pizzaTitle)
-      document.getElementById(pizzaTitle).innerHTML = "В корзине: 0"
+      products.delete(pizzaTitle);
+      document.getElementById(pizzaTitle).innerHTML = "В корзине: 0";
     } else {
-      products.set(pizzaTitle, products.get(pizzaTitle) - 1)
-      document.getElementById(pizzaTitle).innerHTML = "В корзине: " + products.get(pizzaTitle)
+      products.set(pizzaTitle, products.get(pizzaTitle) - 1);
+      document.getElementById(pizzaTitle).innerHTML = "В корзине: " + products.get(pizzaTitle);
     }
-    cost -= pizzaCost
+    cost -= pizzaCost;
+    document.getElementById('basket').innerHTML = showBasket();
+
+    if (products.size <= 0) {
+      document.getElementById("orderBtn").classList.add('unVisibleDisplay');
+    }
   }
-  console.log(products)
+}
+
+function setUnvisible() {
+  document.getElementById("orderFormContainer").classList.add('unVisible');
 }
 
 function orderForm() {
-  document.getElementById("orderForm").classList.remove('unVisible');
-  productString = ""
-  for (let pair of products.entries()) {
-    productString += `${pair[0]} x${pair[1]} \n`
+  if (products.size > 0) {
+
+    document.getElementById("orderFormContainer").classList.remove('unVisible');
+    productString = "";
+    for (let pair of products.entries()) {
+      productString += `${pair[0]} x${pair[1]}`;
+    }
+    document.getElementById("products").innerHTML = showBasket();
+    document.getElementById("cost").innerHTML = "Цена: " + cost + "Руб";
   }
-  document.getElementById("products").innerHTML = "Продукты: " + productString
-  document.getElementById("cost").innerHTML = "Цена: " + cost + "Руб"
 }
 
 async function sendOrder(userId) {
-  let adress = document.getElementById("orderAddress").value;
-
-  order = {
-    "products": productString,
-    "cost": cost,
-    "client": userId,
-    "address": adress,
+  let address = document.getElementById("orderAddress").value;
+  console.log(address.includes(' ', 0))
+  if (!address.includes(' ', 0) && address !== '') {
+    order = {
+      "products": productString,
+      "cost": cost,
+      "client": userId,
+      "address": address,
+    }
+    let response = await fetch('http://localhost:8000/order/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(order)
+    });
+    window.location.replace(response.url); // через контроллер не работает, поэтому переадресация здесь (спросить и исправить)
   }
-  console.log(order)
-  let response = await fetch('http://localhost:8000/order/create', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(order)
-  });
-  // window.location.replace(
-  //   "http://localhost:8000/order/thank"
-  // );
-  document.getElementById("orderForm").classList.add('unVisible');
-  document.getElementById("products").innerHTML = "Продукты:"
-  document.getElementById("cost").innerHTML = "Цена: 0Руб"
-  document.getElementById("orderAddress").value = ""
-  for (let pair of products.entries()) {
-    document.getElementById(pair[0]).innerHTML = "В корзине: 0"
-  }
-  productString = ""
-  products.clear()
-  cost = 0
-  alert("Спасибо за заказ!")
 }
